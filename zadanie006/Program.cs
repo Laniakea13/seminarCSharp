7f9fbe1ee9d0ba978957a558808395ee166fa7ae
Laniakea <vinograd89@rambler.ru>
2021-11-21 22:33:35 +0300

    zadanie 006 done. Чёт/Нечет

---
+++ zadanie006/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить является ли число чётным
+int a = 90;
+int b = 2;
+int d = 0;
+int e = 1;
+int c = (a % b);
+if(c == d) Console.Write($"Число {a} четное");
+if(c == e) Console.Write($"Число {a} нечерное");
