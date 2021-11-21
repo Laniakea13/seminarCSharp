c09becd347130b72c187ee2241d115fbff02e74a
Laniakea <vinograd89@rambler.ru>
2021-11-21 21:10:12 +0300

    zadanie004 done. Максимальное из трех

---
+++ zadanie004/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти максимальное из трех чисел
+int numberA = 56;
+int numberB = 45;
+int numberC = 76;
+int max = 0;
+
+if(numberB > numberA)
+{
+    max = numberB;
+}
+else
+{
+    max = numberA;
+}
+if(max < numberC)
+{
+    max = numberC;
+}
+else
+{
+    max = max;
+}
+Console.Write("Максимальное число из трех - ");
+Console.WriteLine(max);
