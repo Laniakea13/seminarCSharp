ee4bdddbe6bc5b5968ac808e90355c5cb1089db4
Laniakea <vinograd89@rambler.ru>
2021-11-21 20:30:06 +0300

    zadanie003 done Разобрались с днями недели

---
+++ zadanie003/Program.cs
@@ -0,0 +1,10 @@
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите номер дня недели от 1 до 7");
+string den = Console.ReadLine();
+if(den == "1") Console.WriteLine("Это понедельник");
+if(den == "2") Console.WriteLine("Это вторник");
+if(den == "3") Console.WriteLine("Это же среда");
+if(den == "4") Console.WriteLine("Это четверг");
+if(den == "5") Console.WriteLine("Это пятница-развратница :)");
+if(den == "6") Console.WriteLine("Это суббота");
+if(den == "7") Console.WriteLine("Это воскресенье!");
