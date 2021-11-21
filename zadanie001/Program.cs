c4b59545e25692ea964c0acca914deed6934f97a
Laniakea <vinograd89@rambler.ru>
2021-11-21 19:47:28 +0300

    zadanie001 done

---
+++ zadanie001/Program.cs
@@ -0,0 +1,14 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+int numberA = 5;
+int numberB = 24;
+int numberC = numberA *numberA;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+if(numberB == numberC)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}
