3af981f34b4b22c9bf5ba8d76cba47037b64f4af
Laniakea <vinograd89@rambler.ru>
2021-11-21 20:02:56 +0300

    zadanie 002 done

---
+++ zadanie002/Program.cs
@@ -0,0 +1,11 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int numberA = 89;
+int numberB = 45;
+
+int bolshe = numberA;
+
+if(numberA > numberB) bolshe = numberA;
+if(numberA < numberB) bolshe = numberB;
+
+Console.WriteLine(bolshe);
+Console.Write("Это число больше");
