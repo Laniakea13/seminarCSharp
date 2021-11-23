2a7142520ac148980ecf9cd236df53b78c1474f0
Laniakea <vinograd89@rambler.ru>
2021-11-23 22:01:06 +0300

    zadanie 013 done! кратность заданному числу

---
+++ zadanie013/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("ВВедите ваше число");
+
+int numberA = 100;
+int numberB = int.Parse(Console.ReadLine());
+int numberC = numberA % numberB;
+
+if(numberC == 0 )
+{
+  Console.WriteLine("Число кратно заданному");
+}
+else
+{
+  Console.WriteLine("Число не кратно");
+  Console.WriteLine($"Остаток от деления {numberC}");
+}
