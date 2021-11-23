35834e5641337676d3504d9fbcde5f0809eff245
Laniakea <vinograd89@rambler.ru>
2021-11-23 21:06:32 +0300

    zadanie 007 done. n, -n

---
+++ zadanie007.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать числа от -N до N
+int numberN = 10;
+int count = 0;
+int times = 0;
+
+times = numberN*2 +1;
+numberN = numberN*(-1);
+
+while(count<times)
+{
+    Console.Write($" {numberN} ,");
+    numberN = numberN +1;
+    count = count + 1;
+}
