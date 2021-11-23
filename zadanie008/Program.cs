d35a2983fbe6b766dc48094e3f024575e3b27bbc
Laniakea <vinograd89@rambler.ru>
2021-11-23 21:20:18 +0300

    zadanie 008 done. Четные числа от 1 до

---
+++ zadanie008/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать четные числа от 1 до N
+int numberN = 99;
+int count = 0;
+int chetnie =2;
+int schet = numberN - chetnie;
+
+Console.Write("Четные числа:");
+
+while(count <= schet /2)
+{
+    Console.Write($"{chetnie}, ");
+    chetnie = chetnie + 2;
+    count = count +1;
+}
