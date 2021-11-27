b51e72b5bd201349e23aab774718e2fa2d0d8d58
Laniakea <vinograd89@rambler.ru>
2021-11-27 23:39:52 +0300

    016 решено

---
+++ zadanie016/Program.cs
@@ -0,0 +1,6 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Введите порядновый номер дня недели - ");
+int day = int.Parse(Console.ReadLine());
+if(day > 7) Console.Write("В неделе всего 7 дней");
+if(day == 6 ^ day == 7) Console.Write("Поздравляю, это выходной!");
+if(day ==1 | day ==2 | day ==3 | day ==4 | day ==5) Console.Write("Это будний день :(");
