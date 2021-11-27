26176be2e77ed55587f2da699b9e8f68f90f3f17
Laniakea <vinograd89@rambler.ru>
2021-11-27 21:57:46 +0300

    сделала 014

---
+++ zadanie014/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите трехзначное число и я сообщу его третью цифру - ");
+int numberA = int.Parse(Console.ReadLine());
+
+int divider1 = 100;
+int divider2 = 10;
+int numberC = 0;
+double numberB = numberA / divider1;
+int numberN = numberA%divider1;
+if(numberB < 1) 
+{
+    Console.Write("Это двухзначное число :| и соответственно третьей цифры в нем нет");
+}
+else
+{
+    numberC = numberN / 10;
+    Console.Write($"Третья цифра этого числа - {numberC}. Очевидно же!");
+}
