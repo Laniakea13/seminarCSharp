6fc40d466b9acaf91e846f3c881093ff91c7f635
Laniakea <vinograd89@rambler.ru>
2021-11-27 20:15:15 +0300

    модифицировала 006

--- zadanie006/Program.cs
+++ zadanie006/Program.cs
@@ -1,8 +1,9 @@
 ﻿// Выяснить является ли число чётным
-int a = 90;
+Console.Write("Введите любое целое число - ");
+int a = int.Parse(Console.ReadLine());
 int b = 2;
 int d = 0;
 int e = 1;
 int c = (a % b);
-if(c == d) Console.Write($"Число {a} четное");
-if(c == e) Console.Write($"Число {a} нечерное");
+if(c == d) Console.Write($"Число {a} является чётным");
+if(c == e) Console.Write($"Число {a} является нечётным");
