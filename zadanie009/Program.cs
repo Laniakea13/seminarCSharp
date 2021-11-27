8fc51295ab44f71d5c7541c878dce5fe08b6b9da
Laniakea <vinograd89@rambler.ru>
2021-11-27 21:03:34 +0300

    выполнила 009

---
+++ zadanie009/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.Write("Введите любое трехзначное целое число - ");
+int numberA = int.Parse(Console.ReadLine());
+int divider = 100;
+int ostatok = 0;
+double result1 = numberA / divider;
+int result3 = numberA%divider;
+int result2 = result3%10;
+if(result1 < 1) 
+{
+    Console.WriteLine("Это двухзначное число, дружочек");
+}
+else
+{
+    ostatok = result2;
+    Console.WriteLine($"Последней цифрой этого чила является {ostatok}");
+}
+    
+
+
+
