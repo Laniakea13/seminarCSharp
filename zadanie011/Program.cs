cd47f675e69712f5624aedc1d0d0f6c3f68e1d09
Laniakea <vinograd89@rambler.ru>
2021-11-28 10:34:32 +0300

    011 выполнено

--- zadanie011/Program.cs
+++ zadanie011/Program.cs
@@ -1,1 +1,18 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10, 99);
+Console.WriteLine($"Рандомное число - {number}");
+int number1 = number / 10;
+int number2 = number % 10;
+if (number1 > number2)
+{
+    Console.WriteLine($"Наибольшая цифра - {number1}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"Наибольшая цифра - {number2}");
+}
+else
+ {
+    Console.WriteLine("Обе цифры одинаковые");
+ }
