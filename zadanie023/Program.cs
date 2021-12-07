38eaa07363ac0522dceb1389c202a6b14c79a057
Laniakea <vinograd89@rambler.ru>
2021-12-07 21:58:08 +0300

    023 done

---
+++ zadanie023/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("Введите чему будет ровняться N - ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i} в квадрате = {i*i}");
+}
+
