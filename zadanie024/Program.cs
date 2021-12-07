d077899bcdffc38c257ec143b5631ba84da2d847
Laniakea <vinograd89@rambler.ru>
2021-12-07 21:55:42 +0300

    024 done

---
+++ zadanie024/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите чему будет ровняться N - ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i} в кубе = {i*i*i}");
+}
+
