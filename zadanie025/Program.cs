c44c5fc0422554964300e20e52c09150da64f96f
Laniakea <vinograd89@rambler.ru>
2021-12-09 18:28:17 +0300

    start 025, not done yet

---
+++ zadanie025/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти сумму чисел от 1 до А
+Console.WriteLine("Укажите чему будет ровняться А -");
+int A = int.Parse(Console.ReadLine());
+
+int[] arr = new int[A];
+     
+arr[0] = 1;
+int i = 0;
+while (i < A)
+{
+     arr[i] = i + 1;
+     i++;
+}
+i = 0;
+while (i < arr.Length)
+{
+     Console.Write(arr[i] + " ");
+     i++;
+}
+i = 0;
