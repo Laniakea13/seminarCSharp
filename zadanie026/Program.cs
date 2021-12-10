c0dfbcfc481a2b7c40d1af5646bd6c242b38a4b1
Laniakea <vinograd89@rambler.ru>
2021-12-10 21:01:31 +0300

    026 done

---
+++ zadanie026/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.WriteLine("Введите число, которое будем возводить в степень");
+int A = int.Parse(Console.ReadLine());
+int A1 = A;
+Console.WriteLine("В какую сепень будем возводить?");
+int B = int.Parse(Console.ReadLine());
+for (int i = 1; i < B; i++)
+{
+    A = A * A1;
+}
+Console.WriteLine($"Число {A1} в степени {B} равно {A}");
