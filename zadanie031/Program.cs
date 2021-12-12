8472e7341e995707695a8ef41470e70806e099bc
Laniakea <vinograd89@rambler.ru>
2021-12-12 14:47:16 +0300

    031 done

---
+++ zadanie031/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+int[] arr= new int[8];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0, 999);
+    Console.Write(arr[i] + " ");
+
+}
