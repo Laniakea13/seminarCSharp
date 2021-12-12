6df34eed589102390bc039b1059b2debf77fef0e
Laniakea <vinograd89@rambler.ru>
2021-12-12 14:11:56 +0300

    029 done

---
+++ zadanie029/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите чему будет ровняться N");
+int N = int.Parse(Console.ReadLine());
+int rez = 1;
+int[] arr = new int[N];
+     
+arr[0] = 1;
+int i = 0;
+while (i < N)
+{
+     arr[i] = i + 1;
+     rez = rez * arr[i];
+     i++;
+}
+Console.WriteLine($"Произведение чисел от 1 до {N} = {rez}");
