b70a99a9fa5afc912b1dcbef23f4384cef767c20
Laniakea <vinograd89@rambler.ru>
2021-12-12 13:38:36 +0300

    028 done

---
+++ zadanie028/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить количество цифр в числе
+Console.Write("Введите число - ");
+string number = Console.ReadLine();
+double numberX = Convert.ToInt32(number);
+int sum = 0;
+for (int i = 0; i < number.Length; i++)
+{
+    sum = sum +1;
+}
+Console.WriteLine(sum);
