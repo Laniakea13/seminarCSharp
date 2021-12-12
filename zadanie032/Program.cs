addf97895ab8c812bfcd573091a94f03a5d7529f
Laniakea <vinograd89@rambler.ru>
2021-12-12 14:51:45 +0300

    032 done

---
+++ zadanie032/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] arr= new int[8];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0, 2);
+    Console.Write(arr[i] + " ");
+
+}
