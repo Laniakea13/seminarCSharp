7ccb2790d1c58122baa769de944055c7023b5957
Laniakea <vinograd89@rambler.ru>
2021-12-13 22:37:36 +0300

    037 done

---
+++ zadanie037/Program.cs
@@ -0,0 +1,16 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int[] arr = new int[123];
+int count= 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0, 1000);
+    Console.Write(arr[i] + " ");
+}
+for (int j = 0; j < arr.Length; j++)
+{
+    if(arr[j] > 10 & arr[j] < 99) count = count + 1;
+}
+Console.WriteLine();
+Console.Write("Массив совержит следующее количество элементов из отрезка [10,99] - ");
+Console.Write(count);
