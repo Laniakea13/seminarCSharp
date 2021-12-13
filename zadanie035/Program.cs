4ace68cca1b19f49592f29b4124c7b1ea40141e4
Laniakea <vinograd89@rambler.ru>
2021-12-13 22:07:57 +0300

    035 done

---
+++ zadanie035/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+int[] arr = new int[25];
+Console.Write("Какое число будем искать от 1 до 100? Ввод ");
+int N = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0, 100);
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
+for (int j = 0; j < arr.Length; j++)
+{
+    if(arr[j] == N) count = 1;
+}   
+if(count > 0) Console.WriteLine("Это число присутствует в массиве");
+else Console.WriteLine("Искомое число в массиве отсутсвует");
