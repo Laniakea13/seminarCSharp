7950151d7941b8e144e43d430d5a35dd400f503c
Laniakea <vinograd89@rambler.ru>
2021-12-18 11:10:12 +0300

    050 done

---
+++ zadanie050/Program.cs
@@ -0,0 +1,46 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+   for(int i=0; i<matr.GetLength(0); i++)
+   {
+       for (int j=0; j< matr.GetLength(1); j++)
+       {
+           Console.Write($"{matr[i,j]}  ");
+       }
+       Console.WriteLine();
+   }
+}
+void EvenArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if(matr[i,j] % 2 == 0) matr[i,j] = matr[i,j] * -1;
+        }
+    }
+}
+
+Console.Write("Введите значение n -");
+int n= int.Parse(Console.ReadLine());
+Console.Write("Введите значение k -");
+int k= int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int [,] matrix = new int[n,k];
+
+FillArray(matrix);
+PrintArray(matrix);
+EvenArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
