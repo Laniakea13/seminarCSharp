bee6f793195f8d4318022a62846fb5f2f2992b41
Laniakea <vinograd89@rambler.ru>
2021-12-18 10:18:52 +0300

    048 done

---
+++ zadanie048/Program.cs
@@ -0,0 +1,30 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+   for(int i=0; i<matr.GetLength(0); i++)
+   {
+       for (int j=0; j< matr.GetLength(1); j++)
+       {
+           Console.Write($"{matr[i,j]}");
+       }
+       Console.WriteLine();
+   }
+}
+
+
+
+int [,] matr = new int[5,6];
+
+FillArray(matr);
+PrintArray(matr);
+Console.WriteLine();
