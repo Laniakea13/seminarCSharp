12d091125229426259da316a5a227a374acb5c8a
Laniakea <vinograd89@rambler.ru>
2021-12-25 10:28:14 +0300

    066 done

---
+++ zadanie066/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+void Number(int n)
+{
+    
+    if (n  ==1) 
+    {
+        Console.Write();
+    }    
+    else
+    {
+        Number(n-1);
+    
+    }
+    Console.Write($"{n} ");
+}
+Number(10);
