fd5f1f7c1c87f9cfac464c60394711e9f199c0e0
Laniakea <vinograd89@rambler.ru>
2021-12-25 10:49:56 +0300

    068 done

---
+++ zadanie068/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+string Numbers(int m, int n)
+{
+    if (m<=n) return Numbers(m, n-1) + $"{n} ";
+    else return string.Empty;
+    Console.Write($"{n} ");
+}
+Console.WriteLine(Numbers(3,10));
