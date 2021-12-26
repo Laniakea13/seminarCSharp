dd753769d2af56596e6c1fe87fb86da91c7a941a
Laniakea <vinograd89@rambler.ru>
2021-12-26 10:41:15 +0300

    070 done

---
+++ zadanie070/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму цифр числа
+int number = -23;
+
+int SumNumber(int num)
+{
+    if (num < 0) num *= -1;
+    if (num != 0) return num % 10 + SumNumber(num /10);
+    return 0;
+}
+Console.WriteLine(SumNumber(number));
