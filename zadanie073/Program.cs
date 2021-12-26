19d2ee1f7b257f0d13630018e09378629ca7d3ae
Laniakea <vinograd89@rambler.ru>
2021-12-26 11:38:15 +0300

    073 done

---
+++ zadanie073/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+int Function(int a, int b, int n)
+{
+    if(n==1) return a;
+    if(n ==2) return b;
+    
+    return Function(a,b, n-1) + Function(a,b, n-2);
+}
+int count = 10;
+for (int i = 1; i<count; i++)
+{
+    Console.Write($"{Function(1,2,i)} ");
+}
